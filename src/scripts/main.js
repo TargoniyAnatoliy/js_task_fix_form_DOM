5e70a62bbaac3cc62516e70aba45f2657a45fdfb
TargoniyAnatoliy <targoniyanatoliy@gmail.com>
2024-11-19 17:03:09 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const labelName = capitalizeName(input.name);
+
+  input.parentNode.insertAdjacentHTML(
+    'afterbegin',
+    `<label class="field-label" for="${input.id}">${labelName}</label>`,
+  );
+  input.setAttribute('placeholder', labelName);
+}
+
+function capitalizeName(inputName) {
+  let capitalizedName = '';
+
+  for (const ch of inputName) {
+    if (ch === ch.toUpperCase()) {
+      capitalizedName += ' ';
+    }
+    capitalizedName += ch;
+  }
+
+  capitalizedName = capitalizedName[0].toUpperCase() + capitalizedName.slice(1);
+
+  return capitalizedName;
+}
